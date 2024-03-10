7ac75a1dc1dc1c36fe13dc18138ad68bc64d27be
r3dpoint <3207964+r3dpoint@users.noreply.github.com>
2024-03-10 14:46:53 -0700

    wip

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+nvm ls
+
+# set a well known password for jetbrains ssh
+# new_passwd="password"
+# echo -e "${new_passwd}\n${new_passwd}" | sudo passwd "$(whoami)"
+
+# nvm alias default lts/iron
+
+# git config --global branch.autosetupmerge always
+# git config --global branch.autosetuprebase always
+
+# sudo apt-get update && apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
